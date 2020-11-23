ca8777f2ec900f67a8f275894648d1978872ea68
W. Guilherme <ravok123@gmail.com>
2020-11-23 11:39:38 -0300

    add: getLink, hitLink connected into database

--- backend/src/controllers/links.ts
+++ backend/src/controllers/links.ts
@@ -30,25 +30,24 @@ async function postLink(req: Request, res: Response){
    res.status(201).json(link)
 }
 
-function getLink(req: Request, res: Response){
-   // const code = req.params.code as string;
-   // const link = links.find(item=> item.code === code)
-   // if(!link)
-   //    res.sendStatus(404)
-   // else
-   //    res.json(link)
+async function getLink(req: Request, res: Response){
+   const code = req.params.code as string;
+   const link = await linksRepository.findByCode(code);
+   if(!link)
+      res.sendStatus(404)
+   else
+      res.json(link)
 }
 
-function hitLink(req: Request, res: Response){
-   // const code = req.params.code as string;
-   // const index = links.findIndex(item=> item.code === code )
+async function hitLink(req: Request, res: Response){
+   const code = req.params.code as string;
+   const link = await linksRepository.hit(code)
 
-   // if(index === -1)
-   //    res.sendStatus(404)
-   // else
-   // links[index].hits!++
-   //    res.json(links[index])
-}
+   if(!link)
+      res.sendStatus(404)
+   else
+      res.json(link)
+   }
 
 export default {
    postLink,
