46d3ee430b750b897e3a3c4e3254d29a8f688777
W. Guilherme <ravok123@gmail.com>
2020-11-23 11:17:02 -0300

    add: sequelize + connect sequelize + typescript + unified model

---
+++ backend/src/models/linkModel.ts
@@ -0,0 +1,36 @@
+import Sequelize, {Optional, Model} from 'sequelize'
+import {Link} from './link'
+import database from '../database'
+
+// tells when I'll create a link, the ID is optional
+interface ILinkCreationAttributes extends Optional<Link, "id">{}
+
+export interface ILinkModel extends Model<Link, ILinkCreationAttributes>, Link {}
+
+
+// define follows rules from ILinkModel
+const LinkModel = database.define<ILinkModel>('link', {
+   id: {
+      type: Sequelize.INTEGER.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+   },
+   url: {
+      type: Sequelize.STRING(255),
+      allowNull: false
+   },
+   code: {
+      type: Sequelize.STRING(20),
+      unique: true,
+      allowNull: false
+   },
+   hits: {
+      type: Sequelize.INTEGER.UNSIGNED,
+      allowNull: false,
+      defaultValue: 0
+   }
+})
+
+export default LinkModel
+
