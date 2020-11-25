33a8db52ce22209630cbfb10672e6681c99f438e
W. Guilherme <ravok123@gmail.com>
2020-11-25 14:49:54 -0300

    add: input url

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,22 +1,38 @@
 import React from 'react'
 import Header from '../../components/Header'
-import {Container, InputGroup, FormControler, Button, Alert} from 'react-bootstrap'
+import {Container, InputGroup, FormControl, Button, Alert} from 'react-bootstrap'
 import  {ContentContainer, Form} from './styles'
 
 class Home extends React.Component {
    constructor(props){
       super(props);
+
+      this.state = {
+         isLoading: false,
+         url: '',
+         shortenedURL: '',
+         errorMessage: ''
+      }
    }
 
    render(){
       return (
          <Container>
-
-         <Header>Seu novo encurtador de URL. :)</Header>
-         <ContentContainer>
-
-
-         </ContentContainer>
+            <Header>Seu novo encurtador de URL. :)</Header>
+            <ContentContainer>
+               <Form>
+                  <InputGroup>
+                  <FormControl
+                  placeholder="Digite a url para encurtar"
+                  defaultValue=""
+                  onChange={e=>this.setState({url: e.target.value})}
+                  /> 
+                  <InputGroup.Append>  
+                     <Button variant="primary" type="submit">Encurtar</Button>
+                  </InputGroup.Append> 
+                  </InputGroup>
+               </Form>
+            </ContentContainer>
          </Container>
       )
    }
